05dd0500f80f701bf351c728cf61e514b436f27a
depabloguzman <depabloguzman@gmail.com>
2025-07-19 00:49:29 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenidos a la Peña de Pádel - Lunes</h1>
+      <p>Esta es una versión inicial. Próximamente: fixture, grupos, resultados, y puntos.</p>
+    </div>
+  );
+}
+
+export default App;
